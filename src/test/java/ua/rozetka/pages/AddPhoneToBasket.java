cc6647e62fcb2b4be443579b674d56352924e0c2
TtesterR <butko.vl7@gmail.com>
2019-11-12 14:48:44 +0200

    version 3

--- src/test/java/ua/rozetka/pages/AddPhoneToBasket.java
+++ src/test/java/ua/rozetka/pages/AddPhoneToBasket.java
@@ -14,29 +14,29 @@ public class AddPhoneToBasket extends PageObject {
        getDriver().get("https://rozetka.com.ua/");
     }
 
-    @Step ("the user looks up product \"([^\"]*)\"$")
+    @Step ("the user looks up product")
     public void enterKeywords(String keyword) {
         $(ILocators.SEARCH_FIELD).type(keyword);
     }
 
-    @Step ("^the user choose product \"([^\"]*)\"$")
+    @Step ("the user choose product")
     public void clickOnButton() {
         $(ILocators.SEARCH_CLICK).click();
     }
 
-    @Step ("^the user changes the color of the phone to Space gray$")
+    @Step ("the user changes the color of the phone to Space gray")
     public void clickOnSpaceGray() {
         waitFor(waitTime);
         $(ILocators.CLICK_ON_SPACE_GRAY).click();
     }
 
-    @Step ("^The user adds the phone to the basket$")
+    @Step ("the user adds the phone to the basket")
     public void clickOnBasket() {
         waitFor(waitTime);
         $(ILocators.CLICK_ON_BASKET).click();
     }
 
-    @Step ("they should see product to the basket$")
+    @Step ("they should see product to the basket")
     public void addToBasket() {
         String nameWindow = $("//h2[@class='cart-header ng-star-inserted']").getText();
         Assert.assertEquals(nameWindow, "Вы добавили товар в корзину");
